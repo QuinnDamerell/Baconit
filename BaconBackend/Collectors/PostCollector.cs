f05515030248063ad8b5cf225b83fdadb4f89021
vitor <vitor_grs@hotmail.com>
2017-04-15 23:10:55 -0300

    Fixed saved items when there's no comments

--- BaconBackend/Collectors/PostCollector.cs
+++ BaconBackend/Collectors/PostCollector.cs
@@ -115,6 +115,8 @@ namespace BaconBackend.Collectors
                 {
                     // Special case for the saved posts
                     postCollectionUrl = $"/user/{m_baconMan.UserMan.CurrentUser.Name}/saved/.json";
+                    optionalArgs = "type=links";
+
                 }
                 else if (!String.IsNullOrWhiteSpace(collectorContext.forcePostId))
                 {
